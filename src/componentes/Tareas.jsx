5c0e247c35267205fedc53f83b4b1532d36aa972
Thiago Aguero <aguerothiago965@gmail.com>
2026-05-19 20:41:23 -0300

    El boton de cambiar estado de tarea tiene el diseño programado(pero no pasa props). Inicialize variable de prioridades.

--- src/componentes/Tareas.jsx
+++ src/componentes/Tareas.jsx
@@ -1,10 +1,50 @@
+import { useState } from "react";
+
 export default function Tareas(){
+    let [estado, setEstado] = useState(0);
+    let [prioridad, setPrioridad] = useState(0);
+
+    const cambiarEstado = () => {
+        console.log(estado);
+        let nuevoEstado = estado==2 ? 0 : estado+1;
+        setEstado(nuevoEstado);
+    }
+
+    const getContenidoBoton = (dato) => {
+        if (dato == "color"){
+            switch (estado){
+                    case 0:
+                    return "white";
+                case 1:
+                    return "#FFBF4A";
+                case 2:
+                    return "#4FFA00"; 
+            }
+        }
+        if(dato == "texto"){
+            switch (estado){
+                case 0:
+                    return "Pendiente";
+                case 1:
+                    return "En proceso";
+                case 2:
+                    return "Completada"; 
+            }
+        }
+        console.error("dato no reconocido");
+    }
+
+   
+
     return (
         <div className="Tarea">
             <h2>Tarea</h2>
             <textarea name="tarea" id="tarea" cols="30" rows="10">Descripcionnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnn</textarea>
             <div className="TareaPie">
-                <button>Estado</button>
+                <div className="EstadoTarea">
+                    <div className="Punto" style={{ backgroundColor: getContenidoBoton("color") }}></div>
+                    <button style={{color: getContenidoBoton("color")}}onClick={cambiarEstado}>{getContenidoBoton("texto")}</button>
+                </div>
                 <div className="Dias">3 dias</div>
             </div>
         </div>
